6a50d72e6e452435cb6298c57d344076546a15e6
niraj <nirajganesh234@gmail.com>
2022-09-16 13:01:01 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,470 @@
+
+function work_row1()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+       backgroundColor: "#8B8989",
+      };
+      var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+       };
+      var visible_style={
+        visibility:"visible",
+      };
+
+      var display_show={
+         display:"block",
+      }
+      var display_hide={
+          display:"none",
+      }
+      $(".work-number1").css(selected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(unselected_style);
+
+      $(".work1-h4").css(selected_color);
+      $(".work1-h6").css(selected_color);
+      $(".work1-h6").css(visible_style);
+
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+      $(".image-contain1").css(display_show);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_hide);
+}
+
+function work_row2()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+      };
+      var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+        color: "#8B8989",
+      };
+      var visible_style={
+        visibility:"visible",
+      };
+     
+      var display_show={
+        display:"block",
+     }
+     var display_hide={
+         display:"none",
+     }
+
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(selected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(unselected_style);
+
+     
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(selected_color);
+      $(".work2-h6").css(selected_color);
+      $(".work2-h6").css(visible_style);
+      $(".work2-h6").css(visible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_show);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_hide);   
+}
+
+function work_row3()
+{
+    var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };  
+      var invisible_style = {
+        visibility: "hidden",
+       };
+       var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var visible_style={
+        visibility:"visible",
+      };
+
+      var display_show={
+        display:"block",
+     }
+     var display_hide={
+         display:"none",
+     }
+
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(selected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(unselected_style);
+
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(selected_color);
+      $(".work3-h6").css(selected_color);
+      $(".work3-h6").css(visible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_show);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_hide);   
+}
+
+function work_row4()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+       };
+       var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var visible_style={
+        visibility:"visible",
+      };
+
+      var display_show={
+        display:"block",
+      }
+      var display_hide={
+         display:"none",
+      }
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(selected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(unselected_style);
+
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(selected_color);
+      $(".work4-h6").css(selected_color);
+      $(".work4-h6").css(visible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_show);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_hide);       
+}
+
+function work_row5()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+       };
+       var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var visible_style={
+        visibility:"visible",
+      };
+      var display_show={
+        display:"block",
+      }
+      var display_hide={
+         display:"none",
+      }
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(selected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(unselected_style);
+
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(selected_color);
+      $(".work5-h6").css(selected_color);
+      $(".work5-h6").css(visible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_show);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_hide);      
+}
+
+function work_row6()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };  
+      var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+       };
+      var visible_style={
+        visibility:"visible",
+      };
+      var display_show={
+        display:"block",
+      }
+      var display_hide={
+         display:"none",
+      }
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(selected_style);
+      $(".work-number7").css(unselected_style);
+
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(selected_color);
+      $(".work6-h6").css(selected_color);
+      $(".work6-h6").css(visible_style);
+      $(".work7-h4").css(unselected_color);
+      $(".work7-h6").css(invisible_style);
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_show);
+      $(".image-contain7").css(display_hide);       
+}
+
+function work_row7()
+{
+      var selected_style = {
+        backgroundColor : "#000000",
+      };
+      var unselected_style = {
+        backgroundColor: "#8B8989",
+      };
+      var selected_color = {
+        color : "#000000",
+      };
+      var unselected_color = {
+       color: "#8B8989",
+      };
+      var invisible_style = {
+        visibility: "hidden",
+       };
+      var visible_style={
+        visibility:"visible",
+      };
+      var display_show={
+        display:"block",
+      }
+      var display_hide={
+         display:"none",
+      }
+      $(".work-number1").css(unselected_style);
+      $(".work-number2").css(unselected_style);
+      $(".work-number3").css(unselected_style);
+      $(".work-number4").css(unselected_style);
+      $(".work-number5").css(unselected_style);
+      $(".work-number6").css(unselected_style);
+      $(".work-number7").css(selected_style);
+
+      $(".work1-h4").css(unselected_color);
+      $(".work1-h6").css(invisible_style);
+      $(".work2-h4").css(unselected_color);
+      $(".work2-h6").css(invisible_style);
+      $(".work3-h4").css(unselected_color);
+      $(".work3-h6").css(invisible_style);
+      $(".work4-h4").css(unselected_color);
+      $(".work4-h6").css(invisible_style);
+      $(".work5-h4").css(unselected_color);
+      $(".work5-h6").css(invisible_style);
+      $(".work6-h4").css(unselected_color);
+      $(".work6-h6").css(invisible_style);
+      $(".work7-h4").css(selected_color);
+      $(".work7-h6").css(selected_color);
+      $(".work7-h6").css(visible_style);
+
+      $(".image-contain1").css(display_hide);
+      $(".image-contain2").css(display_hide);
+      $(".image-contain3").css(display_hide);
+      $(".image-contain4").css(display_hide);
+      $(".image-contain5").css(display_hide);
+      $(".image-contain6").css(display_hide);
+      $(".image-contain7").css(display_show);       
+}
+
+
+function arrow_click()
+{
+    var display_style = 
+    {
+        display:"block",
+        position:"relative",
+        right:"40px",
+        transition:"2s",
+    };
+    var display_hide=
+    {
+        display:"none",
+    }
+
+    var service1=document.getElementById('service1');
+    var service2=document.getElementById('service2');
+    var service3=document.getElementById('service3'); 
+
+    if(service1.style.display=='block')
+    {
+        $(".service1").css(display_hide);
+        $(".service2").css(display_style);
+        $(".service3").css(display_hide);
+        return;
+    }
+
+    if(service2.style.display=='block')
+    {
+        $(".service1").css(display_hide);
+        $(".service2").css(display_hide);
+        $(".service3").css(display_style);
+        return;
+    }
+
+    if(service3.style.display=='block')
+    {
+        $(".service1").css(display_style);
+        $(".service2").css(display_hide);
+        $(".service3").css(display_hide);
+        return;
+    }
+
+   
+}
